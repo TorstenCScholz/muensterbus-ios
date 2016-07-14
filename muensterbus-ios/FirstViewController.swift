61e7fef05b4df83eebc7b9b314740fce3657c63d
RolandKaiser <rolandkaiser@schwarzgelb.net>
2016-07-14 19:20:16 +0200

    Initial commit

---
+++ muensterbus-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  muensterbus-ios
+//
+//  Created by RolandKaiser on 14.07.16.
+//  Copyright © 2016 Metabus. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
